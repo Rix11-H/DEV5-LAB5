fc7cb460098d71c4ee815d2602616638807bbcee
Rix11-H <60402558+Rix11-H@users.noreply.github.com>
2022-11-22 18:05:31 +0100

    add cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const messagesRouter = require('./routes/messages');
@@ -8,16 +9,24 @@ mongoose.connect('mongodb://localhost:27017/dev5-chat');
 const app = express();
 const port = 3000;
 
+app.use(cors({
+  origin: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'UPDATE'],
+  credentials: true
+}));
+
 // view engine setup
 app.set('view engine', 'jade');
 
 app.use(express.json());
 
 app.use('/', indexRouter);
-// app.use('/users', usersRouter);
+app.use('/users', usersRouter);
 app.use('/api/v1/messages', messagesRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening at ${port}`);
 });
+
+module.exports = app;
 
