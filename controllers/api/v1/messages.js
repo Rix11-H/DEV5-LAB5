bb711ed0f8921cafced2987f4d1aa8414ed756bf
Rix11-H <60402558+Rix11-H@users.noreply.github.com>
2022-11-25 16:30:21 +0100

    feat: add update and delete requests

--- controllers/api/v1/messages.js
+++ controllers/api/v1/messages.js
@@ -1,37 +1,54 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const messageSchema = new Schema({
-    message: String,
-    sender: String,
-    receiver: String,
-    date: Date,
-});
-const Message = mongoose.model('Message', messageSchema);
-
+const Message = require('../../../models/Message');
 
 const getAllMessages = (req, res) => {
-    Message.find({}, (err, messages) => {
-        if(!err) {
-            res.json({
-                "status": "success",
-                "data": {
-                    "messages": messages
+        if(req.query.sender) {
+            Message.find({"sender": req.query.sender}, (err, messages) => {
+                if (!err) {
+                    res.json({
+                        "status": "success",
+                        "message": `Messages from ${req.query.sender} retrieved successfully`,
+                        "data": {
+                            "messages": messages
+                        }
+                    });
+                }
+            });
+        } else {
+            Message.find({}, (err, messages) => {
+                if (!err) {
+                    res.json({
+                        "status": "success",
+                        "message": "All messages retrieved successfully",
+                        "data": {
+                            "messages": messages
+                        }
+                    });
                 }
             });
         }
-    })
-}
+    };
 
 const createMessage = (req, res) => {
     let message = new Message();
-    message.message = "this is a message";
-    message.sender = "Max";
-    message.receiver = "Jan";
+    message.message = req.body.message;
+    message.sender = req.body.sender;
+    message.receiver = req.body.receiver;
     message.date = new Date();
     message.save((err, message) => {
+        if (err) {
+            res.json({
+                "status": "error",
+                "message": "Message not saved",
+                "data": {
+                    "message": message
+                }
+            });
+        }
+
         if (!err) {
             res.json({
                 "status": "success",
+                "message": "Message is created",
                 "data": {
                     "message": message
                 }
@@ -45,6 +62,7 @@ const getMessageById = (req, res) => {
         if (!err) {
             res.json({
                 "status": "success",
+                "message": `Getting message with id = ${req.params.id}`,
                 "data": {
                     "message": message
                 }
@@ -54,36 +72,41 @@ const getMessageById = (req, res) => {
 }
 
 const updateMessage = (req, res) => {
-    Message.findById(req.params.id, (err, message) => {
-        if (!err) {
-            message.message = req.body.message;
-            message.sender = req.body.sender;
-            message.receiver = req.body.receiver;
-            message.date = req.body.date;
-            message.save((err, message) => {
-                if (!err) {
-                    res.json({
-                        "status": "success",
-                        "data": {
-                            "message": message
-                        }
-                    });
-                }
-            });
-        }
+    let messageId = req.params.id;
+    Message.findOneAndUpdate({
+        _id: messageId
+    }, {
+        "message" : req.body.message,
+        "sender" : req.params.sender,
+        "receiver" : req.params.receiver,
+        "date" : new Date()
+    }).then(message => {
+        res.json({
+            "status": "success",
+            "message": `Message with id = ${messageId} is updated`,
+            "data": {
+                "message": message
+            }
+        });
+    }).catch(err => {
+        res.json(err);
     });
 }
 
 const deleteMessage = (req, res) => {
-    Message.findByIdAndRemove(req.params.id, (err, message) => {
-        if (!err) {
-            res.json({
-                "status": "success",
-                "data": {
-                    "message": message
-                }
-            });
-        }
+    let messageId = req.params.id;
+    Message.findOneAndDelete({
+        _id: messageId
+    }).then(message => {
+        res.json({
+            "status": "success",
+            "message": `Message with id = ${messageId} is deleted`,
+            "data": {
+                "message": message
+            }
+        });
+    }).catch(err => {
+        res.json(err);
     });
 }
 
