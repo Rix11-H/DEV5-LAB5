146a3909a7350dfc715e69925a8e34b9bfef63ce
Rix11-H <60402558+Rix11-H@users.noreply.github.com>
2022-11-25 12:44:39 +0100

    feat: fresh start from new express generator

--- app.js
+++ app.js
@@ -1,32 +1,41 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const indexRouter = require('./routes/index');
-const messagesRouter = require('./routes/messages');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-mongoose.connect('mongodb://localhost:27017/dev5-chat');
-const app = express();
-const port = 3000;
-
-app.use(cors({
-  origin: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'UPDATE'],
-  credentials: true
-}));
+var app = express();
 
 // view engine setup
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(logger('dev'));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/api/v1/messages', messagesRouter);
 
-app.listen(port, () => {
-  console.log(`Example app listening at ${port}`);
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 module.exports = app;
-
