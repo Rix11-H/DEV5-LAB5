dd64cd33cfb7b5933063a99febae1adb166536b4
Rix11-H <60402558+Rix11-H@users.noreply.github.com>
2022-11-25 15:33:06 +0100

    feat: add routes for getById, put and delete

--- controllers/api/v1/messages.js
+++ controllers/api/v1/messages.js
@@ -1,20 +1,94 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const messageSchema = new Schema({
+    message: String,
+    sender: String,
+    receiver: String,
+    date: Date,
+});
+const Message = mongoose.model('Message', messageSchema);
+
+
 const getAllMessages = (req, res) => {
-    res.json({
-        "status": "success",
-        "data": {
-            "messages": []
+    Message.find({}, (err, messages) => {
+        if(!err) {
+            res.json({
+                "status": "success",
+                "data": {
+                    "messages": messages
+                }
+            });
+        }
+    })
+}
+
+const createMessage = (req, res) => {
+    let message = new Message();
+    message.message = "this is a message";
+    message.sender = "Max";
+    message.receiver = "Jan";
+    message.date = new Date();
+    message.save((err, message) => {
+        if (!err) {
+            res.json({
+                "status": "success",
+                "data": {
+                    "message": message
+                }
+            });
         }
     });
 }
 
-const createMessage = (req, res) => {
-    res.json({
-        "status": "success",
-        "data": {
-            "message": "Message received"
+const getMessageById = (req, res) => {
+    Message.findById(req.params.id, (err, message) => {
+        if (!err) {
+            res.json({
+                "status": "success",
+                "data": {
+                    "message": message
+                }
+            });
+        }
+    });
+}
+
+const updateMessage = (req, res) => {
+    Message.findById(req.params.id, (err, message) => {
+        if (!err) {
+            message.message = req.body.message;
+            message.sender = req.body.sender;
+            message.receiver = req.body.receiver;
+            message.date = req.body.date;
+            message.save((err, message) => {
+                if (!err) {
+                    res.json({
+                        "status": "success",
+                        "data": {
+                            "message": message
+                        }
+                    });
+                }
+            });
+        }
+    });
+}
+
+const deleteMessage = (req, res) => {
+    Message.findByIdAndRemove(req.params.id, (err, message) => {
+        if (!err) {
+            res.json({
+                "status": "success",
+                "data": {
+                    "message": message
+                }
+            });
         }
     });
 }
 
 module.exports.getAllMessages = getAllMessages;
 module.exports.createMessage = createMessage;
+module.exports.getMessageById = getMessageById;
+module.exports.updateMessage = updateMessage;
+module.exports.deleteMessage = deleteMessage;
--- routes/api/v1/messages.js
+++ routes/api/v1/messages.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const messagesController = require('../../../controllers/api/v1/messages');
 
 router.get('/', messagesController.getAllMessages);
-
 router.post("/", messagesController.createMessage);
+router.get('/:id', messagesController.getMessageById);
+router.put('/:id', messagesController.updateMessage);
+router.delete('/:id', messagesController.deleteMessage);
 
 module.exports = router;
