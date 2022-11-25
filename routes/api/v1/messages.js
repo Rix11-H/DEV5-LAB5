b59aec4ef1f28b54239038c4b864a89f1be1af8f
Rix11-H <60402558+Rix11-H@users.noreply.github.com>
2022-11-25 12:54:49 +0100

    feat: start new messagerouter

---
+++ routes/api/v1/messages.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.json({
+        "status": "success",
+        "data": {
+            "messages": []
+        }
+    })
+});
+
+router.post("/", (req, res) => {
+    res.json({
+        "status": "success",
+        "data": {
+            "message": "Message received"
+        }
+    });
+});
+
+module.exports = router;
